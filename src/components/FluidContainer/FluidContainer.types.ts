1ae5d3a49cabddaf9498762cc94f29f4d54d4299
Jesus Bossa <x@jesusbossa.dev>
2021-05-17 11:16:34 -0500

    chore(card): container type improved

--- src/components/FluidContainer/FluidContainer.types.ts
+++ src/components/FluidContainer/FluidContainer.types.ts
@@ -3,5 +3,5 @@ import { className } from '../../types';
 
 export interface FluidContainerType extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  className: className;
+  className?: className;
 }
