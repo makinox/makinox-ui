1279414ffc40260ade7f23fa1726ebae099808c4
Jesus Bossa <x@jesusbossa.dev>
2021-05-29 17:43:59 -0500

    feat(theme): add dropdown dark mode

--- src/components/Dropdown/Dropdown.styles.ts
+++ src/components/Dropdown/Dropdown.styles.ts
@@ -2,18 +2,44 @@ import styled from 'styled-components';
 
 export const DropdownHiden = styled.div`
   box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
-  background-color: rgba(var(--light-background));
   flex-direction: column;
   position: absolute;
   border-radius: 3px;
   display: none;
+
+  ${(props) => {
+    if (props.theme?.isDark) {
+      return `
+        background-color: rgb(var(--dark-background));
+        color: rgb(var(--dark-onBackground));
+      `;
+    } else {
+      return `
+        background-color: rgb(var(--light-background));
+        color: rgb(var(--light-onBackground));
+      `;
+    }
+  }}
 `;
 
 export const DropdownContainer = styled.div`
   position: relative;
+
   &:hover > ${DropdownHiden} {
     display: flex !important;
   }
+
+  ${(props) => {
+    if (props.theme?.isDark) {
+      return `
+        color: rgb(var(--dark-onBackground));
+      `;
+    } else {
+      return `
+        color: rgb(var(--light-onBackground));
+      `;
+    }
+  }}
 `;
 
 export const DropdownItem = styled.span`
--- src/stories/Dropdown/Dropdown.stories.tsx
+++ src/stories/Dropdown/Dropdown.stories.tsx
@@ -1,4 +1,5 @@
 import { Story } from '@storybook/react/types-6-0';
+import { ThemeProvider } from 'styled-components';
 import { DropdownType } from '../../types';
 import { Dropdown } from '../../';
 
@@ -11,4 +12,17 @@ export const Default: Story<DropdownType> = (props: DropdownType) => <Dropdown {
 Default.args = {
   Item: <span>Item</span>,
   HiddenItems: [<span>Item</span>, <span>Item</span>, <span>Item</span>],
+};
+
+export const DarkDefault: Story<DropdownType> = (props: DropdownType) => (
+  <ThemeProvider theme={{ isDark: true }}>
+    <Dropdown {...props} />
+  </ThemeProvider>
+);
+DarkDefault.args = {
+  Item: <span>Item</span>,
+  HiddenItems: [<span>Item</span>, <span>Item</span>, <span>Item</span>],
+};
+DarkDefault.parameters = {
+  backgrounds: { default: 'dark' },
 };
