5cf71339a724a0e76136dbc9e8768155a6e08f7f
Jesus Bossa <x@jesusbossa.dev>
2022-08-21 13:21:38 -0500

    chore: update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,6 @@
 module.exports = {
   staticDirs: ['../public'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
 };
