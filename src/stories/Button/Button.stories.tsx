9f7041be62baebc166d81c777c5814515ee261fe
Jesus Bossa <x@jesusbossa.dev>
2022-09-26 19:29:18 -0500

    chore: update button documentation

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { GiCastle } from 'react-icons/gi';
+import { Story } from '@storybook/react/types-6-0';
 import { ButtonContained, ButtonOutline, ButtonText, ButtonNeumorphic } from '../..';
 
 export default {
@@ -6,48 +7,100 @@ export default {
   component: <button />,
 };
 
-export const Contained = () => <button className={ButtonContained()}>contained</button>;
+export const Contained: Story = (args) => <button className={ButtonContained(args)}>{args.text}</button>;
+Contained.args = {
+  css: {},
+  isDark: false,
+  text: 'contained',
+};
 
-export const Outlined = () => <button className={ButtonOutline()}>Outlined</button>;
+export const Outlined: Story = (args) => <button className={ButtonOutline(args)}>{args.text}</button>;
+Outlined.args = {
+  css: {},
+  isDark: false,
+  text: 'Outlined',
+};
 
-export const Text = () => <button className={ButtonText()}>Text</button>;
+export const Text: Story = (args) => <button className={ButtonText(args)}>{args.text}</button>;
+Text.args = {
+  css: {},
+  isDark: false,
+  text: 'Text',
+};
 
-export const Neumorphic = () => <button className={ButtonNeumorphic()}>Text</button>;
+export const Neumorphic: Story = (args) => <button className={ButtonNeumorphic(args)}>{args.text}</button>;
+Neumorphic.args = {
+  css: {},
+  isDark: false,
+  text: 'Neumorphic',
+};
 
-export const ContainedIcon = () => (
-  <button className={ButtonContained()}>
+export const ContainedIcon: Story = (args) => (
+  <button className={ButtonContained(args)}>
     <GiCastle />
   </button>
 );
+ContainedIcon.args = {
+  css: {},
+  isDark: false,
+};
 
-export const OutlinedIcon = () => (
-  <button className={ButtonOutline()}>
+export const OutlinedIcon: Story = (args) => (
+  <button className={ButtonOutline(args)}>
     <GiCastle />
   </button>
 );
+OutlinedIcon.args = {
+  css: {},
+  isDark: false,
+};
 
-export const TextIcon = () => (
-  <button className={ButtonText()}>
+export const TextIcon: Story = (args) => (
+  <button className={ButtonText(args)}>
     <GiCastle />
   </button>
 );
+TextIcon.args = {
+  css: {},
+  isDark: false,
+};
 
-export const DarkContained = () => <button className={ButtonContained({ isDark: true })}>contained</button>;
+export const DarkContained: Story = (args) => <button className={ButtonContained(args)}>{args.text}</button>;
 DarkContained.parameters = {
   backgrounds: { default: 'dark' },
 };
+DarkContained.args = {
+  css: {},
+  isDark: true,
+  text: 'contained',
+};
 
-export const DarkOutlined = () => <button className={ButtonOutline({ isDark: true })}>Outlined</button>;
+export const DarkOutlined: Story = (args) => <button className={ButtonOutline(args)}>{args.text}</button>;
 DarkOutlined.parameters = {
   backgrounds: { default: 'dark' },
 };
+DarkOutlined.args = {
+  css: {},
+  isDark: true,
+  text: 'Outlined',
+};
 
-export const DarkText = () => <button className={ButtonText({ isDark: true })}>Text</button>;
+export const DarkText: Story = (args) => <button className={ButtonText(args)}>{args.text}</button>;
 DarkText.parameters = {
   backgrounds: { default: 'dark' },
 };
+DarkText.args = {
+  css: {},
+  isDark: true,
+  text: 'Text',
+};
 
-export const DarkNeumorphic = () => <button className={ButtonNeumorphic({ isDark: true })}>Text</button>;
+export const DarkNeumorphic: Story = (args) => <button className={ButtonNeumorphic(args)}>{args.text}</button>;
 DarkNeumorphic.parameters = {
   backgrounds: { default: 'dark' },
+};
+DarkNeumorphic.args = {
+  css: {},
+  isDark: true,
+  text: 'Neumorphic',
 };
